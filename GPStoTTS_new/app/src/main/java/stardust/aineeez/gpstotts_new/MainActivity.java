9f387fa6ee3c7f1ed49cd079ecd7d110869e9784
Ines Wendler <ines.wendler@s2015.tu-chemnitz.de>
2018-07-01 15:13:29 +0200

    First commit - working app without gps and tts

---
+++ GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/MainActivity.java
@@ -0,0 +1,31 @@
+package stardust.aineeez.gpstotts_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button talkBtn = (Button) findViewById(R.id.talkBtn);
+        //R steht für Ressourcen, "i" tippen, dann aus der Liste wählen, wo meine im Layout-Fenster erstellten Buttons etc. stehen
+        //findViewById ist genau dazu da, diese Ressource zu suchen
+
+        talkBtn.setOnClickListener(new View.OnClickListener() {  //hier definiere ich was passieren soll, wenn man draufklickt auf den Button
+
+            @Override
+            public void onClick(View v) { //das ist ein OnClick-Event, innerhalb der Klammern definieren, was passieren soll
+                //GPS-Daten auslesen fehlt hier
+
+                    TextView coordinateResultTextView = (TextView) findViewById(R.id.coordinateResultTextView);
+                    coordinateResultTextView.setText("Breitengrad: 50.83592, Längengrad: 12.92331");
+            }
+        });
+    }
+}
