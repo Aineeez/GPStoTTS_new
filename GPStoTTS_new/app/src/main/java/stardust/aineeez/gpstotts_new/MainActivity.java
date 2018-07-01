14b3357c872d0e62fc60198e163a3465f82f99e8
Ines Wendler <ines.wendler@s2015.tu-chemnitz.de>
2018-07-01 17:29:09 +0200

    läuft wie zuvor, weiterer Button show/talk hinzugefügt, Verknüpfung zu ShowLocationActivity fehlt noch

--- GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/MainActivity.java
+++ GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/MainActivity.java
@@ -1,20 +1,11 @@
 package stardust.aineeez.gpstotts_new;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.app.Activity;
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Button talkBtn = (Button) findViewById(R.id.talkBtn);
+        Button showBtn = (Button) findViewById(R.id.showBtn);
         //R steht für Ressourcen, "i" tippen, dann aus der Liste wählen, wo meine im Layout-Fenster erstellten Buttons etc. stehen
         //findViewById ist genau dazu da, diese Ressource zu suchen
 
-        talkBtn.setOnClickListener(new View.OnClickListener() {  //hier definiere ich was passieren soll, wenn man draufklickt auf den Button
+        showBtn.setOnClickListener(new View.OnClickListener() {  //hier definiere ich was passieren soll, wenn man draufklickt auf den Button
 
             @Override
             public void onClick(View v) { //das ist ein OnClick-Event, innerhalb der Klammern definieren, was passieren soll
