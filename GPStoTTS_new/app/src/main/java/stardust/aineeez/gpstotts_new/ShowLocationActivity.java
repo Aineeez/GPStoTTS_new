19531d53688050f27c7670d29ef0c9f7ce49c76e
Ines Wendler <ines.wendler@s2015.tu-chemnitz.de>
2018-07-10 16:36:59 +0200

    lauffähige Version mit neuem Code zu Permission Requests, der noch nichts tut

--- GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/ShowLocationActivity.java
+++ GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/ShowLocationActivity.java
@@ -9,12 +9,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShowLocationActivity extends Activity implements LocationListener {
-    private TextView latituteField;
+    private TextView latitudeField;
     private TextView longitudeField;
     private LocationManager locationManager;
     private String provider;
@@ -24,8 +23,18 @@ public class ShowLocationActivity extends Activity implements LocationListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_location);
-        latituteField = (TextView) findViewById(R.id.TextView02);
+        latitudeField = (TextView) findViewById(R.id.TextView02);
         longitudeField = (TextView) findViewById(R.id.TextView04);
+
+        //aus diesem Tutorial: https://youtu.be/scySXsk9yRc
+        final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
+        final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 102;
+        boolean permissionIsGranted = false;
+
+
+        String [] requestedPermissions = new String[2]; // das habe ich angelegt, weil beim Abfragen der Berechtigungen mit ActivityCompat.requestPermissions
+        requestedPermissions[0] = "android.Manifest.permission.ACCESS_FINE_LOCATION"; //vom Nutzer ein String-Array übergeben werden muss
+        requestedPermissions[1] = "android.Manifest.permission.ACCESS_COARSE_LOCATION";
 
         // Get the location manager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -43,6 +52,9 @@ public class ShowLocationActivity extends Activity implements LocationListener {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this, requestedPermissions, MY_PERMISSIONS_REQUEST_FINE_LOCATION); //das ist aus der Doku, nicht aus dem YT-Tutorial
+
             return;
         }
         Location location = locationManager.getLastKnownLocation(provider); //lässt sich nur ausführen, wenn der permission check
@@ -53,8 +65,8 @@ public class ShowLocationActivity extends Activity implements LocationListener {
             System.out.println("Provider " + provider + " has been selected.");
             onLocationChanged(location);
         } else {
-            latituteField.setText("Location not available");
-            longitudeField.setText("Location not available");
+            latitudeField.setText("Fucking Location not available");
+            longitudeField.setText("Fucking Location not available");
         }
     }
 
@@ -86,7 +98,7 @@ public class ShowLocationActivity extends Activity implements LocationListener {
     public void onLocationChanged(Location location) {
         int lat = (int) (location.getLatitude());
         int lng = (int) (location.getLongitude());
-        latituteField.setText(String.valueOf(lat));
+        latitudeField.setText(String.valueOf(lat));
         longitudeField.setText(String.valueOf(lng));
     }
 
@@ -107,5 +119,37 @@ public class ShowLocationActivity extends Activity implements LocationListener {
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Disabled provider " + provider,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+
+            case 101: //das ist der Code, der für FINE_LOCATION verwendet wird
+
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //permission was granted
+               // permissionIsGranted = true;
+                Toast.makeText(this, "Permission for GPS was granted",
+                        Toast.LENGTH_SHORT).show();
+
+
+                 }
+                else{
+                    //permission was denied
+               //     permissionIsGranted = false;
+                    Toast.makeText(this, "Permission for GPS was denied",
+                            Toast.LENGTH_SHORT).show();
+                    latitudeField.setText("permission not granted");
+                    longitudeField.setText("permission not granted");
+                }
+
+                break;
+
+            case 102: //nicht so klar: im Tutorial stand hier MY_PERMISSIONS_REQUEST_COARSE_LOCATION, aber das wird hier nicht erkannt
+                //do something
+                break;
+        }
     }
 }
