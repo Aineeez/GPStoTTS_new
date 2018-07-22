0dd5e21ebcfec1c9b3ef203a60d5889672cb2efd
Ines Wendler <ines.wendler@s2015.tu-chemnitz.de>
2018-07-22 15:15:56 +0200

    gps weitgehend implementiert, nicht laufen gelassen

--- GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/ShowLocationActivity.java
+++ GPStoTTS_new/app/src/main/java/stardust/aineeez/gpstotts_new/ShowLocationActivity.java
@@ -8,15 +8,70 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class ShowLocationActivity extends Activity implements LocationListener {
     private TextView latitudeField;
     private TextView longitudeField;
     private LocationManager locationManager;
     private String provider;
+    private LocationRequest locationRequest;
+    long request_Interval = 1000; //nach 1 Sekunde werden neue GPS-Daten abgefragt
+
+    //
+    class LocationSettingsOnFailureListener implements OnFailureListener {
+        int REQUEST_CHECK_SETTINGS = 1;
+        Activity activity;
+
+        //neuer Konstruktor, damit ich Activity übergeben kann, denn sonst kann ich nicht
+        //auf die Klasse über der Klasse zugreifen
+        LocationSettingsOnFailureListener(Activity activity){
+            this.activity = activity;
+        }
+
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            if (e instanceof ResolvableApiException) //
+                try {
+                    //Cast, damit ich die Methoden der ResolvalbeApiExceptions nutzen kann
+                    ResolvableApiException exception = (ResolvableApiException) e;
+
+                    exception.startResolutionForResult(this.activity, REQUEST_CHECK_SETTINGS);
+                } catch (Exception f) {
+                    //do nothing --> die behebbaren Fehler wurden oben schon abgefangen
+                    //nicht behebbare Fehler kann ich auch nicht behandeln
+                }
+        }
+    }
+
+    class LocationRequestResultCallback extends LocationCallback{
+
+        ShowLocationActivity activity;
+
+        LocationRequestResultCallback(ShowLocationActivity activity){
+            this.activity = activity
+
+        }
+
+    }
+
 
     /** Called when the activity is first created. */
     @Override
@@ -26,66 +81,66 @@ public class ShowLocationActivity extends Activity implements LocationListener {
         latitudeField = (TextView) findViewById(R.id.TextView02);
         longitudeField = (TextView) findViewById(R.id.TextView04);
 
-        //aus diesem Tutorial: https://youtu.be/scySXsk9yRc
-        final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
-        final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 102;
-        boolean permissionIsGranted = false;
+        this.locationRequest = LocationRequest.create();
+        this.locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        this.locationRequest.setInterval(request_Interval);
+
+        //Einstellungen, wie die Arbeit des Requests dem User angezeigt werden, z. B. GPS-Symbol leuchtet
+        //fragt User, ob GPS aktiviert werden soll
+        //Konstruktor für LocationsSettingsRequest ist nicht frei verfügbar, daher Builder
+        LocationSettingsRequest.Builder locationSettingsRequestBuilder = new LocationSettingsRequest.Builder();
+
+        //der Builder muss vor dem eigentlich Request ans Betriebssystem prüfen, ob der Nutzer seine
+        //Permission gegeben hat, falls nicht, wird ggf. Dialog zur Nachfrage beim Nutzer getriggert
+        locationSettingsRequestBuilder.addLocationRequest(this.locationRequest);
+        locationSettingsRequestBuilder.setAlwaysShow(true); //true, denn GPS ist unbedingt nötig für die App
+
+        //der Builder baut jetzt einen echten LocationsSettingsRequest
+        LocationSettingsRequest locationSettingsRequest= locationSettingsRequestBuilder.build();
+
+        //führt den Request aus, Client ist schnittstelle zu service
+        //Client ist Vermittler zwischen App und Betriebssystem (einem Service)
+        //mein Client wird angefragt bei den Location Services des Betriebssystems
+        // this ist die Instanz der Klasse in der ich mich befinde (Show Location Activity)
+        //this muss übergeben werden, damit dieser Client in dieser Activity genutzt werden kann
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+
+        //jetzt fragt settingsClient bei OS an, ob die Einstellungen so sind, dass der Request gestellt werden kann
+        //Task = Aufgabe, führt etwas aus und antwortet mit dem, was in spitzen Klammern definiert ist
+        //Task arbeitet sofort nach Initialisierung, ohne dass man ihn aufrufen muss
+        //die returnte response ist mir egal, ich will hören, wenn ein Fehler auftritt
+        Task<LocationSettingsResponse> checkLocationSettings = settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        //um den Parameter zu übergeben, musste ich oben die entsprechende Klasse anlegen
+        checkLocationSettings.addOnFailureListener(new LocationSettingsOnFailureListener(this));
 
 
-        String [] requestedPermissions = new String[2]; // das habe ich angelegt, weil beim Abfragen der Berechtigungen mit ActivityCompat.requestPermissions
-        requestedPermissions[0] = "android.Manifest.permission.ACCESS_FINE_LOCATION"; //vom Nutzer ein String-Array übergeben werden muss
-        requestedPermissions[1] = "android.Manifest.permission.ACCESS_COARSE_LOCATION";
-
-        // Get the location manager
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // Define the criteria how to select the locatioin provider -> use default
-        Criteria criteria = new Criteria(); //als Auswahlkriterium wird der Default genommen (unklar, was der Default ist)
-        //Doku: https://developer.android.com/reference/android/location/Criteria
-
-        provider = locationManager.getBestProvider(criteria, false); //wählt aus, welche Quelle die beste ist für die GPS-Daten
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
-            //ActivityCompat.requestPermissions(this, requestedPermissions, MY_PERMISSIONS_REQUEST_FINE_LOCATION); //das ist aus der Doku, nicht aus dem YT-Tutorial
-
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(provider); //lässt sich nur ausführen, wenn der permission check
-        // in der IF direkt obendrüber ausgeführt wurde
-
-        // Initialize the location fields
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-        } else {
-            latitudeField.setText("Fucking Location not available");
-            longitudeField.setText("Fucking Location not available");
-        }
-    }
 
     /* Request updates at startup */
+
+    //hierzu life cycle von activities berücksichtigen
+        //GPS-Request muss in onResume rein, da es dann wieder ausgeführt wird, auch wenn die App
+        //mal im Hintergrund lief oder eine andere Activity geöffnet wurde
     @Override
     protected void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            super.onResume();
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+
+            //Request wird nicht direkt ausgeführt sondern immer an einen Client übergeben
+            FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+            //immer wenn neue GPS-Daten kommen, wird ausgeführt, was in locationRequestResultCallback definiert ist
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, null);
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
-    }
 
     /* Remove the locationlistener updates when Activity is paused */
     @Override
